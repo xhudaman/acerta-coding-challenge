ac2578bda5053b4bef8a16bc49b519cbfd3f927b
xhudaman <edell.matthew@gmail.com>
2022-11-21 14:27:22 -0500

    Refactored to clean up and fixed styles

--- web/src/pages/customers/id.tsx
+++ web/src/pages/customers/id.tsx
@@ -15,8 +15,13 @@ import { BsChevronExpand } from "react-icons/bs";
 
 const Customer = () => {
   const { id } = useParams();
-  const navigate = useNavigate();
   const [fruitsState, setFruitsState] = useState<FruitWithMinMax[] | []>([]);
+  const [selectedFruit, setSelectedFruit] = useState<Fruit | null>(null);
+  const totalMinFruits = useRef<HTMLInputElement>(null);
+  const totalMaxFruits = useRef<HTMLInputElement>(null);
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
   const {
     data: customer,
     isLoading: customerLoading,
@@ -25,6 +30,7 @@ const Customer = () => {
     queryKey: ["customers", id],
     queryFn: () => getCustomer(id),
   });
+
   const {
     data: fruits,
     isLoading: fruitsLoading,
@@ -33,9 +39,6 @@ const Customer = () => {
     queryKey: ["fruits"],
     queryFn: () => getFruits(),
   });
-
-  const totalMinFruits = useRef<HTMLInputElement>(null);
-  const totalMaxFruits = useRef<HTMLInputElement>(null);
 
   const filteredFruit = useMemo(() => {
     if (fruits) {
@@ -48,6 +51,7 @@ const Customer = () => {
   }, [fruits, fruitsState]);
 
   useEffect(() => {
+    // Load default fruitsState from customer
     if (customer && customer.fruits?.length > 0) {
       const customerFruits = customer.fruits.map((fruit) => ({
         ...fruit,
@@ -58,10 +62,6 @@ const Customer = () => {
       setFruitsState(customerFruits);
     }
   }, [customer]);
-
-  const [selectedFruit, setSelectedFruit] = useState<Fruit | null>(null);
-
-  const queryClient = useQueryClient();
 
   const mutation = useMutation({
     mutationFn: (customer: CustomerType) => {
@@ -77,11 +77,7 @@ const Customer = () => {
     },
   });
 
-  if (customerLoading || fruitsLoading) return <div>Loading...</div>;
-  if (customerError) return <div>Error loading customer</div>;
-  if (fruitsError) return <div>Error loading fruits</div>;
-
-  const handleMinMaxInpputChange = (
+  const handleFruitPreferenceChange = (
     fruit: FruitWithMinMax,
     key: string,
     value: number
@@ -120,6 +116,7 @@ const Customer = () => {
       const minTotalFruit = totalMinFruits.current?.value
         ? parseInt(totalMinFruits.current?.value)
         : customer.minTotalFruit;
+
       const maxTotalFruit = totalMaxFruits.current?.value
         ? parseInt(totalMaxFruits.current?.value)
         : customer.maxTotalFruit;
@@ -139,167 +136,176 @@ const Customer = () => {
       mutation.mutate(updatedCustomer);
     }
   };
+
+  if (customerLoading || fruitsLoading) return <div>Loading...</div>;
+  if (customerError) return <div>Error loading customer</div>;
+  if (fruitsError) return <div>Error loading fruits</div>;
+
   return (
     <div className="flex flex-col justify-center items-center w-full max-w-2xl py-6">
       <h2 className="text-2xl font-bold">{customer?.name}</h2>
       <form
-        className="flex flex-col space-y-2 w-full justify-center items-center"
+        className="flex flex-col space-y-8 w-full justify-center items-center"
         onSubmit={handleSubmit}
       >
-        <h3 className="text-xl font-semibold mb-2 mr-auto">
-          Total Fruit in Basket
-        </h3>
-        <div className="flex w-full space-x-4">
-          <div className="flex flex-col w-full justify-center items-center">
-            <label htmlFor="min-total" className="mr-auto">
-              Min
-            </label>
-            <input
-              ref={totalMinFruits}
-              type="number"
-              name="min-total"
-              id="min-total"
-              className="border border-black rounded w-full px-2"
-              defaultValue={customer?.minTotalFruit}
-            />
-          </div>
-          <div className="flex flex-col justify-center items-center w-full">
-            <label htmlFor="max-total" className="mr-auto">
-              Max
-            </label>
-            <input
-              ref={totalMaxFruits}
-              type="number"
-              name="max-total"
-              id="max-total"
-              className="border border-black rounded w-full px-2"
-              defaultValue={customer?.maxTotalFruit}
-            />
+        <div className="w-full">
+          <h3 className="text-xl font-semibold mb-2 mr-auto">
+            Total Fruit in Basket
+          </h3>
+          <div className="flex w-full space-x-4">
+            <div className="flex flex-col w-full justify-center items-center">
+              <label htmlFor="min-total" className="mr-auto">
+                Min
+              </label>
+              <input
+                ref={totalMinFruits}
+                type="number"
+                name="min-total"
+                id="min-total"
+                className="border border-black rounded w-full px-2"
+                defaultValue={customer?.minTotalFruit}
+              />
+            </div>
+            <div className="flex flex-col justify-center items-center w-full">
+              <label htmlFor="max-total" className="mr-auto">
+                Max
+              </label>
+              <input
+                ref={totalMaxFruits}
+                type="number"
+                name="max-total"
+                id="max-total"
+                className="border border-black rounded w-full px-2"
+                defaultValue={customer?.maxTotalFruit}
+              />
+            </div>
           </div>
         </div>
-        <h3 className="text-xl font-semibold mb-2 mr-auto">
-          Fruit Preferences
-        </h3>
-        <div className="flex justify-center items-center w-full space-x-4">
-          {filteredFruit && (
-            <Listbox value={selectedFruit} onChange={setSelectedFruit}>
-              <div className="relative mt-1 w-1/2">
-                <div className="flex justify-center items-center space-x-4">
-                  <Listbox.Button className="flex bg-gray-200 px-4 py-2 rounded w-full text-left items-center">
-                    {selectedFruit?.name || "Select Fruit"}
-                    <BsChevronExpand className="ml-auto text-xl" />
-                  </Listbox.Button>
-                  <button
-                    type="button"
-                    className="rounded bg-gray-400 px-4 py-2 hocus:bg-gray-600 text-white"
-                    onClick={handleAddFruit}
-                  >
-                    <FaPlus className="text-2xl" />
-                  </button>
-                </div>
-                <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                  {filteredFruit.map((fruit) => (
-                    <Listbox.Option
-                      key={fruit.id}
-                      className={({ active }) =>
-                        `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                          active
-                            ? "bg-amber-100 text-amber-900"
-                            : "text-gray-900"
-                        }`
-                      }
-                      value={fruit}
+        <div className="w-full space-y-4">
+          <h3 className="text-xl font-semibold mb-2 mr-auto">
+            Fruit Preferences
+          </h3>
+          <div className="flex justify-center items-center w-full space-x-4">
+            {filteredFruit && (
+              <Listbox value={selectedFruit} onChange={setSelectedFruit}>
+                <div className="relative mt-1 w-1/2">
+                  <div className="flex justify-center items-center space-x-4">
+                    <Listbox.Button className="flex bg-gray-200 px-4 py-2 rounded w-full text-left items-center">
+                      {selectedFruit?.name || "Select Fruit"}
+                      <BsChevronExpand className="ml-auto text-xl" />
+                    </Listbox.Button>
+                    <button
+                      type="button"
+                      className="rounded bg-gray-400 px-4 py-2 hocus:bg-gray-600 text-white"
+                      onClick={handleAddFruit}
                     >
-                      {({ selected }) => (
-                        <span
-                          className={`block truncate ${
-                            selected ? "font-medium" : "font-normal"
-                          }`}
-                        >
-                          {fruit.name}
-                        </span>
-                      )}
-                    </Listbox.Option>
-                  ))}
-                </Listbox.Options>
-              </div>
-            </Listbox>
-          )}
-        </div>
-        <table className="w-full max-w-2xl table-auto border-collapse">
-          <thead>
-            <tr className="bg-gray-400">
-              <th className="border border-black text-left px-4">ID</th>
-              <th className="border border-black text-left px-4">Name</th>
-              <th className="border border-black text-left px-4">Min</th>
-              <th className="border border-black text-left px-4">Max</th>
-              <th className="border border-black text-left px-4">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {fruitsState?.map((fruit) => (
-              <tr key={fruit.id}>
-                <td className="border border-black text-center px-4">
-                  {fruit.id}
-                </td>
-                <td className="border border-black text-center px-4">
-                  {fruit.name}
-                </td>
-                <td className="border border-black text-center px-4">
-                  <label htmlFor={`min-${fruit.name}`} className="hidden">
-                    Min
-                  </label>
-                  <input
-                    type="number"
-                    name={`min-${fruit.name}`}
-                    id={`min-${fruit.name}`}
-                    className="border border-black rounded w-full px-2"
-                    value={fruit.min}
-                    onChange={({ target }) =>
-                      handleMinMaxInpputChange(
-                        fruit,
-                        "min",
-                        parseInt(target.value)
-                      )
-                    }
-                  />
-                </td>
-                <td className="border border-black text-center px-4">
-                  <label htmlFor={`max-${fruit.name}`} className="hidden">
-                    Max
-                  </label>
-                  <input
-                    type="number"
-                    name={`max-${fruit.name}`}
-                    id={`max-${fruit.name}`}
-                    className="border border-black rounded w-full px-2"
-                    value={fruit.max}
-                    onChange={({ target }) =>
-                      handleMinMaxInpputChange(
-                        fruit,
-                        "max",
-                        parseInt(target.value)
-                      )
-                    }
-                  />
-                </td>
-                <td className="border border-black text-center px-4 py-2">
-                  <button
-                    className="bg-red-600 p-2 rounded text-white hocus:bg-red-500"
-                    type="button"
-                    onClick={() => handleDeleteFruit(fruit.id)}
-                  >
-                    Delete
-                  </button>
-                </td>
+                      <FaPlus className="text-2xl" />
+                    </button>
+                  </div>
+                  <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                    {filteredFruit.map((fruit) => (
+                      <Listbox.Option
+                        key={fruit.id}
+                        className={({ active }) =>
+                          `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                            active
+                              ? "bg-amber-100 text-amber-900"
+                              : "text-gray-900"
+                          }`
+                        }
+                        value={fruit}
+                      >
+                        {({ selected }) => (
+                          <span
+                            className={`block truncate ${
+                              selected ? "font-medium" : "font-normal"
+                            }`}
+                          >
+                            {fruit.name}
+                          </span>
+                        )}
+                      </Listbox.Option>
+                    ))}
+                  </Listbox.Options>
+                </div>
+              </Listbox>
+            )}
+          </div>
+          <table className="w-full max-w-2xl table-auto border-collapse">
+            <thead>
+              <tr className="bg-gray-400">
+                <th className="border border-black text-left px-4">ID</th>
+                <th className="border border-black text-left px-4">Name</th>
+                <th className="border border-black text-left px-4">Min</th>
+                <th className="border border-black text-left px-4">Max</th>
+                <th className="border border-black text-left px-4">Actions</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {fruitsState?.map((fruit) => (
+                <tr key={fruit.id}>
+                  <td className="border border-black text-center px-4">
+                    {fruit.id}
+                  </td>
+                  <td className="border border-black text-center px-4">
+                    {fruit.name}
+                  </td>
+                  <td className="border border-black text-center px-4">
+                    <label htmlFor={`min-${fruit.name}`} className="hidden">
+                      Min
+                    </label>
+                    <input
+                      type="number"
+                      name={`min-${fruit.name}`}
+                      id={`min-${fruit.name}`}
+                      className="border border-black rounded w-full px-2"
+                      value={fruit.min}
+                      onChange={({ target }) =>
+                        handleFruitPreferenceChange(
+                          fruit,
+                          "min",
+                          parseInt(target.value)
+                        )
+                      }
+                    />
+                  </td>
+                  <td className="border border-black text-center px-4">
+                    <label htmlFor={`max-${fruit.name}`} className="hidden">
+                      Max
+                    </label>
+                    <input
+                      type="number"
+                      name={`max-${fruit.name}`}
+                      id={`max-${fruit.name}`}
+                      className="border border-black rounded w-full px-2"
+                      value={fruit.max}
+                      onChange={({ target }) =>
+                        handleFruitPreferenceChange(
+                          fruit,
+                          "max",
+                          parseInt(target.value)
+                        )
+                      }
+                    />
+                  </td>
+                  <td className="border border-black text-center px-4 py-2">
+                    <button
+                      className="bg-red-600 p-2 rounded text-white hocus:bg-red-500"
+                      type="button"
+                      onClick={() => handleDeleteFruit(fruit.id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
 
         <button
           type="submit"
-          className="rounded bg-green-500 hocus:bg-green-700 w-1/4"
+          className="rounded bg-green-600 hocus:bg-green-400 w-1/4 px-4 py-2 text-white"
         >
           Save
         </button>
